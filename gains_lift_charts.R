a1bd723da8b399652947cc001e472ce44036b5f6
PeerChristensen <hr.pchristensen@gmail.com>
2019-02-20 14:45:54 +0100

    add response plot

--- gains_lift_charts.R
+++ gains_lift_charts.R
@@ -6,17 +6,27 @@ myL <- as.vector(as.character(aml@leaderboard$model_id))[1]
 gains_lift <- h2o.getModel(myL) %>%
   h2o.gainsLift()
 
-# lift
-gains_lift %>%
-  ggplot(aes(x=cumulative_data_fraction,y=cumulative_lift)) +
-  geom_line(size = .8) +
-  geom_point(size = 1)
-
-# gains (called capture rate)
+# Gains (called capture rate)
 gains_lift %>%
   ggplot(aes(x=cumulative_data_fraction,y=cumulative_capture_rate)) +
   geom_line(size = .8) +
-  geom_point(size = 1)
+  geom_point(size = 1) +
+  geom_segment(aes(x=0,y=0,xend = 1, yend = 1),size = 1,linetype = 2,col='grey')+
+  ggtitle("Gains chart",
+          subtitle = "When we apply the model and select x % of customers,\nwhat % of the target class observations can we expect to hit?") +
+  theme(plot.title = element_text(size = 16),
+        plot.subtitle = element_text(size = 14, face = "italic",vjust=-1))
+
+# Lift 
+gains_lift %>%
+  ggplot(aes(x=cumulative_data_fraction,y=cumulative_lift)) +
+  geom_line(size = .8) +
+  geom_point(size = 1) +
+  geom_segment(aes(x=0,y=1,xend = 1, yend = 1),size = 1,linetype = 2,col='grey')+
+ggtitle("Lift chart",
+          subtitle = "When we apply the model and select x % of customers,\nhow many times better is that than using no model?") +
+  theme(plot.title = element_text(size = 16),
+        plot.subtitle = element_text(size = 14, face = "italic",vjust=-1))
  
 gains_lift %>% 
   as_tibble() %>%
