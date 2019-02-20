d059bf5f37950d6a5acb8cc635df4d1aea5ca454
PeerChristensen <hr.pchristensen@gmail.com>
2019-02-20 15:36:39 +0100

    add plotting function

--- attempt_roc_from_autoML.R
+++ attempt_roc_from_autoML.R
@@ -47,3 +47,55 @@ df %>%
   theme(plot.title    = element_text(size = 16),
         plot.subtitle = element_text(size = 12,face="italic")) +
   scale_colour_viridis_d("Model")
+
+
+### function for preparing ROC curves for plotting
+
+roc_curves <- function(H2OAutoML_object, plot = F) {
+  
+  models <- as.vector(as.character(H2OAutoML_object@leaderboard$model_id)) %>% 
+    map(h2o.getModel)
+
+  df <- tibble()
+  
+  for (i in 1:length(models)) {
+    
+    perf <- h2o.performance(models[[i]])
+    tpr  <- perf@metrics$thresholds_and_metric_scores$tpr
+    fpr  <- perf@metrics$thresholds_and_metric_scores$fpr
+    
+    model_id  <- models[[i]]@model_id
+    algorithm <- models[[i]]@algorithm
+    
+    d <- tibble(model_id,algorithm,tpr,fpr)
+    d <- add_row(d,model_id = model_id, algorithm=algorithm,tpr=0,fpr=0,.before=T)
+    d <- add_row(d,model_id = model_id, algorithm=algorithm,tpr=0,fpr=0,.before=F)
+    
+    df <- rbind(df,d)
+  }
+  
+  if (plot == T) {
+    p <- df %>% 
+      ggplot(aes(fpr,tpr,colour = model_id)) + 
+      geom_line(size = 1,alpha=.8) +
+      geom_segment(aes(x=0,y=0,xend = 1, yend = 1),linetype = 2,col='grey', size =1) +
+      coord_fixed() +
+      xlab('False Positive Rate') +
+      ylab('True Positive Rate') +
+      ggtitle('ROC-kurver',
+              subtitle = "Sammenligning af de bedste modeller") +
+      theme_light() +
+      theme(plot.title    = element_text(size = 16),
+            plot.subtitle = element_text(size = 12,face="italic")) +
+      scale_colour_viridis_d("Model")
+    
+    print(p)
+    
+    return(df)
+    
+  } 
+  else {
+    
+    return(df)
+  }
+}
--- gains_lift_charts.R
+++ gains_lift_charts.R
@@ -29,11 +29,14 @@ ggtitle("Lift chart",
         plot.subtitle = element_text(size = 14, face = "italic",vjust=-1))
  
 # Response
+prop_target <- prop.table(table(df$Churn))[2]
+
 gains_lift %>%
   ggplot(aes(x=cumulative_data_fraction,y=cumulative_response_rate)) +
   geom_line(size = .8) +
   geom_point(size = 1) +
-  geom_segment(aes(x=0,y=0.26,xend = 1, yend = 0.26),size = 1,linetype = 2,col='grey') +
+  geom_segment(aes(x=0,y = prop_target,xend = 1, yend = prop_target),size = 1,linetype = 2,col='grey') +
+  ylim(c(0,1)) +
   ggtitle("Response chart",
           subtitle = "When we apply the model and select x % of customers, \nwhat is the expected % of target class observations in the selection?") +
   theme(plot.title = element_text(size = 16),
