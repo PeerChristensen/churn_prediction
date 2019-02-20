1bb4c198a2b778803e6ce60bbbf604d32668e7c7
PeerChristensen <hr.pchristensen@gmail.com>
2019-02-20 14:46:25 +0100

    + response

--- gains_lift_charts.R
+++ gains_lift_charts.R
@@ -28,10 +28,22 @@ ggtitle("Lift chart",
   theme(plot.title = element_text(size = 16),
         plot.subtitle = element_text(size = 14, face = "italic",vjust=-1))
  
+# Response
+gains_lift %>%
+  ggplot(aes(x=cumulative_data_fraction,y=cumulative_response_rate)) +
+  geom_line(size = .8) +
+  geom_point(size = 1) +
+  geom_segment(aes(x=0,y=0.26,xend = 1, yend = 0.26),size = 1,linetype = 2,col='grey') +
+  ggtitle("Response chart",
+          subtitle = "When we apply the model and select x % of customers, \nwhat is the expected % of target class observations in the selection?") +
+  theme(plot.title = element_text(size = 16),
+        plot.subtitle = element_text(size = 14, face = "italic",vjust=-1))
+
+
 gains_lift %>% 
   as_tibble() %>%
   select(cumulative_data_fraction,cumulative_lift,cumulative_capture_rate) %>%
   gather(key = "metric", value = "value", - cumulative_data_fraction) %>%
   ggplot(aes(x=cumulative_data_fraction,y=value)) +
   geom_line() +
-  facet_wrap(~metric,scales = "free") +
+  facet_wrap(~metric,scales = "free")
