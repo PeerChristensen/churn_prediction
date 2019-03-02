3a55d46bab3b6c269ea047acabd7a4fc2b2992ec
PeerChristensen <hr.pchristensen@gmail.com>
2019-03-02 20:14:25 +0100

    dummycoding added to data set

--- preprocess_churn_data.R
+++ preprocess_churn_data.R
@@ -2,6 +2,9 @@
 # clean telco churn file
 
 library(tidyverse)
+library(caret)
+library(h2o)
+library(mlr)
 
 df <- read_csv("telco.csv") %>%
   select(-customerID,-SeniorCitizen) %>%
@@ -9,4 +12,24 @@ df <- read_csv("telco.csv") %>%
   mutate_if(is.character, as.factor) %>%
   mutate_if(is.numeric, scale,scale=T,center=T)
 
-write_csv("telecom_churn_prep.csv")
+df <- createDummyFeatures(df, target = "Churn")
+
+write_csv(df,"telecom_churn_prep.csv")
+
+index <- createDataPartition(df$Churn, p = 0.7, list = FALSE)
+
+train_data <- df[index, ]
+val_test_data  <- df[-index, ]
+
+index2 <- createDataPartition(val_test_data$Churn, p = 0.5, list = FALSE)
+
+valid_data <- val_test_data[-index2, ]
+test_data  <- val_test_data[index2, ]
+
+train_hf <- as.h2o(train_data)
+valid_hf <- as.h2o(valid_data)
+test_hf  <- as.h2o(test_data)
+
+outcome <- "Churn"
+predictors <- setdiff(names(train_hf), outcome)
+
